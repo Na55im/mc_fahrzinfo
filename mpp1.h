82aec4cef5de1e881d83d58f425b3b0301753648
Norman <e.n.stetter@gmail.com>
2017-10-28 04:07:36 -0700

    initial commit

---
+++ mpp1.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "utils/uartstdio.h"
+#include "driverlib/uart.h"
